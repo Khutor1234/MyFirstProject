5dd02b2657687bf78dfb2c7e409afca76e7b0bab
Alexandra Khutorna <khutornaalexandra@gmail.com>
2020-12-02 14:17:01 +0200

    My first project about movies

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-"use strict";
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false,
+}
 
-const answer = prompt("18?","");
-console.log(typeof(answer));
+let a = prompt("Какой фильм вы смотрели последним?", ""),
+    b = prompt("Как вы его оцените?", ""),
+    c = prompt("Какой фильм вы смотрели последним?", ""),
+    d = prompt("Как вы его оцените?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(numberOfFilms);
+console.log(personalMovieDB);
+console.log(personalMovieDB.movies);
+console.log(personalMovieDB.count);
